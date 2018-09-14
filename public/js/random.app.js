af9998db2c4ac6d202592670768660a1a63617c6
aapresna <aapresna@indiana.edu>
2018-09-14 18:51:20 -0500

    Retrieves JSON data from Randomuser.me

---
+++ public/js/random.app.js
@@ -0,0 +1,74 @@
+var randomApp = new Vue({
+  el: '#random',
+  data: {
+      results:  [
+        {
+          gender: '',
+          name: {
+            title: '',
+            first: '',
+            last: ''
+          },
+          location: {
+            street: '',
+            city: '',
+            state: '',
+            postcode: '',
+            coordinates: {
+              latitude: '',
+              longitude: ''
+            },
+            timezone: {
+              offset: '',
+              description: ''
+            }
+          },
+          email: '',
+          login: {
+            uuid: '',
+            username: '',
+            password: '',
+            salt: '',
+            md5: '',
+            sha1: '',
+            sha256: ''
+          },
+          dob: {
+            date: '',
+            age: 0
+          },
+          registered: {
+            date: '',
+            age: 0
+          },
+          phone: '',
+          cell: '',
+          id: {
+            name: '',
+            value: ''
+          },
+          picture: {
+            large: '',
+            medium: '',
+            thumbnail: ''
+          },
+          nat: ''
+        }
+      ],
+      info: {
+        seed: '',
+        results: 0,
+        page: 0,
+        version: ''
+      }
+    }
+  }
+);
+const url = 'https://randomuser.me/api/?inc=picture,name,nat,dob,email&noinfo';
+fetch(url)
+.then( response => response.json() )
+.then( json => {randomApp.results = json} )
+.catch( err => {
+  console.log('TASK FETCH ERROR:');
+  console.log(err);
+})
