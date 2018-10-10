e94bebcd5a7a57021d7ddeb2aa7871c8b78b061c
aapresna <aapresna@indiana.edu>
2018-10-10 03:30:32 -0400

    Testing comment.php

--- public/api/comment.php
+++ public/api/comment.php
@@ -2,16 +2,20 @@
 
 require '../../app/common.php';
 
-$Id = intval($_GET['Id'] ?? 0);
+// $Id = intval($_GET['Id'] ?? 0);
+//
+// if ($Id < 1) {
+//   throw new Exception('Invalid ID');
+// }
+//
+// // 1. Go to the database and get all work associated with the $taskId
+// $workArr = Work::getWorkById($Id);
+// // 2. Convert to JSON
+// $json = json_encode($workArr, JSON_PRETTY_PRINT);
+// // 3. Print
+// header('Content-Type: application/json');
+// echo $json;
 
-if ($Id < 1) {
-  throw new Exception('Invalid ID');
-}
+$text = "Hello";
 
-// 1. Go to the database and get all work associated with the $taskId
-$workArr = Work::getWorkById($Id);
-// 2. Convert to JSON
-$json = json_encode($workArr, JSON_PRETTY_PRINT);
-// 3. Print
-header('Content-Type: application/json');
-echo $json;
+echo $text;
