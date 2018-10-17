8855fdb219ecef22c09e98b45323f6348543132e
aapresna <aapresna@indiana.edu>
2018-10-17 01:32:19 -0400

    Update public/js/comments.app.js

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -1,43 +1,17 @@
-var tasksApp = new Vue({
-  el: '#taskMain',
+var commentsApp = new Vue({
+  el: '#commentsMain',
   data: {
-    task: {
+    comments: {
       id: 0,
-      title: 'foo',
-      type : '',
-      size : '',
-      team : '',
-      status: '',
-      start_date: '',
-      close_date: null,
-      hours_worked: '',
-      perc_complete: '',
-      current_sprint : ''
+      comment: '',
     },
     work: [ ],
     workForm: { },   // populated by this.getEmptyWorkForm()
     teamList: [] // All the teams
   },
-  computed: {
-    workSpan () {
-      return moment(this.workForm.stop + ' ' + this.workForm.stop_time)
-             .diff(moment(this.workForm.start + ' ' + this.workForm.start_time), 'hours', true)
-             .toFixed(1);
-    }
-  },
+
   methods: {
     handleWorkForm(e) {
-
-      // TODO: Check validity in a better way
-      if (this.workSpan <= 0) {
-        console.error('Cannot submit, invalid values');
-        return;
-      }
-
-      this.workForm.start_date = this.workForm.start + ' ' + this.workForm.start_time;
-      this.workForm.hours = this.workSpan;
-      // Stop field not used by the API
-      // this.workForm.stop_date = this.workForm.stop + ' ' + this.workForm.stop_time;
 
       const s = JSON.stringify(this.workForm);
 
@@ -61,60 +35,31 @@ var tasksApp = new Vue({
       // Reset workForm
       this.workForm = this.getEmptyWorkForm();
     },
-    sumHours() {
-      return this.work.reduce( (sum, current) => sum + current.hours, 0 )
-    },
-    diffAsHours() {
-      return 0 //moment().duration(end.diff(startTime)).asHours();
-    },
+
     getEmptyWorkForm() {
       return {
-        start: moment().format('YYYY-MM-DD'),
-        start_time: moment().format('HH:mm'),
-        stop: moment().format('YYYY-MM-DD'),
-        stop_time: moment().format('HH:mm'),
-        team_id: null,
-        task_id: this.task.id,
-        completion_estimate: 0
+        id: 0,
+        comment: null,
       }
     },
-    prettyDate(d) {
-      return moment(d).format('YYYY-MM-DD HH:mm')
-    }
   },
   created () {
 
     // Do data fetch
-    const url = new URL(window.location.href);
-    const taskId = url.searchParams.get('taskId');
-    console.log('Task: '+ taskId);
-    this.task.id = taskId;
-
-    if (!taskId) {
-      //TODO: Error? 404?
-      //e.g., window.location = '404.html';
-    }
+    const url = new URL("http://ec2-34-226-150-61.compute-1.amazonaws.com/api/comment.php");
+    console.log(url);
 
     // Populate workForm with default values
     this.workForm = this.getEmptyWorkForm();
 
     // TODO: Fetch task-specific data
     // fetch('api/task?id=4')
-    fetch('api/work.php?taskId='+taskId)
+    fetch('api/comment.php')
     .then( response => response.json() )
-    .then( json => {tasksApp.work = json} )
+    .then( json => {commentsApp.work = json} )
     .catch( err => {
       console.error('WORK FETCH ERROR:');
       console.error(err);
-    })
-
-    // Fetch all teams, for the form
-    fetch('api/team.php')
-    .then( response => response.json() )
-    .then( json => {tasksApp.teamList = json} )
-    .catch( err => {
-      console.log('TEAM LIST ERROR:');
-      console.log(err);
     })
   }
 })
