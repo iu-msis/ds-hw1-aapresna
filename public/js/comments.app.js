8fa2f5786b6faa662d25ba2dcc49b9fce5a85d38
aapresna <aapresna@indiana.edu>
2018-10-17 12:04:16 -0400

    Undid cleaning

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -1,56 +1,65 @@
 var commentsApp = new Vue({
   el: '#commentsMain',
   data: {
-      comment: [],
-      newCommentForm: {}
+    comments: {
+      id: 0,
+      comment: '',
     },
+    work: [ ],
+    workForm: { },   // populated by this.getEmptyWorkForm()
+    commentList: [] // All the teams
+  },
 
   methods: {
     handleWorkForm(e) {
 
-this.newCommentForm=this.newCommentForm.comment;
+      const s = JSON.stringify(this.workForm);
+
+      console.log(s);
+
       // POST to remote server
       fetch('api/comment.php', {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         headers: {
             "Content-Type": "application/json; charset=utf-8"
         },
-        body: JSON.stringify(
-          {comment:this.newCommentForm.comment}) // body data type must match "Content-Type" header
+        body: s // body data type must match "Content-Type" header
       })
       .then( response => response.json() )
-      .then( json => {this.comment.push(json)})
+      .then( json => {this.work.push(json)})
       .catch( err => {
         console.error('WORK POST ERROR:');
         console.error(err);
       })
 
       // Reset workForm
-      // this.workForm = this.getEmptyWorkForm();
-    },
-
-
-    getAllComments: function() {
-      fetch('api/comment.php')
-      .then(response => response.json())
-      .then(json => {commentsApp.comment = json;})
-      .catch( err => {
-        console.error('FETCH ERROR:');
-        console.error(err);
-      })
+      this.workForm = this.getEmptyWorkForm();
     },
 
     getEmptyWorkForm() {
       return {
+        id: 0,
         comment: null,
       }
     },
   },
-
   created () {
 
-this.getAllComments();
-this.newCommentForm = this.getEmptyWorkForm();
+    // Do data fetch
+    const url = new URL('http://ec2-34-226-150-61.compute-1.amazonaws.com/api/comment.php');
+    console.log(url);
 
+    // Populate workForm with default values
+    this.workForm = this.getEmptyWorkForm();
+
+    // TODO: Fetch task-specific data
+    // fetch('api/task?id=4')
+    fetch('api/comment.php')
+    .then( response => response.json() )
+    .then( json => {commentsApp.work = json} )
+    .catch( err => {
+      console.error('WORK FETCH ERROR:');
+      console.error(err);
+    })
   }
 })
