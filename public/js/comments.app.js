eaf421645aaf9ec09f610a5ff614e410f001e288
aapresna <aapresna@indiana.edu>
2018-10-17 23:17:01 -0500

    Testing

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -1,21 +1,12 @@
 var commentsApp = new Vue({
   el: '#commentsMain',
   data: {
-    comments: {
-      id: 0,
-      comment: '',
+      comment: [],
+      newCommentForm: {}
     },
-    work: [ ],
-    workForm: { },   // populated by this.getEmptyWorkForm()
-    commentList: [] // All the teams
-  },
 
   methods: {
     handleWorkForm(e) {
-
-      const s = JSON.stringify(this.workForm);
-
-      console.log(s);
 
       // POST to remote server
       fetch('api/comment.php', {
@@ -23,43 +14,42 @@ var commentsApp = new Vue({
         headers: {
             "Content-Type": "application/json; charset=utf-8"
         },
-        body: s // body data type must match "Content-Type" header
+        body: JSON.stringify(
+          {comment:this.newCommentForm.comment}) // body data type must match "Content-Type" header
       })
       .then( response => response.json() )
-      .then( json => {this.work.push(json)})
+      .then( json => {this.comment.push(json)})
       .catch( err => {
         console.error('WORK POST ERROR:');
         console.error(err);
       })
 
       // Reset workForm
-      this.workForm = this.getEmptyWorkForm();
+      // this.workForm = this.getEmptyWorkForm();
+    },
+
+
+    getAllComments: function() {
+      fetch('api/comment.php')
+      .then(response => response.json())
+      .then(json => {commentsApp.comment = json;})
+      .catch( err => {
+        console.error('FETCH ERROR:');
+        console.error(err);
+      })
     },
 
     getEmptyWorkForm() {
       return {
-        id: 0,
         comment: null,
       }
     },
   },
+
   created () {
 
-    // Do data fetch
-    const url = new URL('http://ec2-34-226-150-61.compute-1.amazonaws.com/api/comment.php');
-    console.log(url);
+this.getAllComments();
+this.newCommentForm = this.getEmptyWorkForm();
 
-    // Populate workForm with default values
-    this.workForm = this.getEmptyWorkForm();
-
-    // TODO: Fetch task-specific data
-    // fetch('api/task?id=4')
-    fetch('api/comment.php')
-    .then( response => response.json() )
-    .then( json => {commentsApp.work = json} )
-    .catch( err => {
-      console.error('WORK FETCH ERROR:');
-      console.error(err);
-    })
   }
 })
